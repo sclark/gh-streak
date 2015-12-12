70b7b5bc39a9001c373182a8048d9cd567146af5
Stephen Clark <stephen@californiaclarks.com>
2015-12-12 16:51:45 -0500

    Updated styles and created game page

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,11 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'gh-streak' });
+  res.render('index');
+});
+
+router.get('/game/:game', function(req, res, next) {
+  res.render('game', {game: {name: "stever-2", players: [{name: "sclark", streak: "7 days", status: "good"}, {name: "ianlo", streak: "7 days", status: "warn"}]}});
 });
 
 module.exports = router;
